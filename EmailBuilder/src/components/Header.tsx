b0349f3be1149c6e4322e602a4fec9b94895c9a5
pawelkgith <pawelkielbasa500@gmail.com>
2026-03-11 14:32:53 +0100

    Dodano i ostylowano przycisk contact

--- EmailBuilder/src/components/Header.tsx
+++ EmailBuilder/src/components/Header.tsx
@@ -1,9 +1,14 @@
 import styles from '../scss/Header.module.scss';
 
 function Header() {
+    const handleGithubRedirect = () => {
+        open("https://github.com/pawelkgith/reactportfolio", "_blank");
+    }
+
     return (
         <div className={styles.header}>
             <h1>Email Builder</h1>
+            <button className={styles.projectsBtn} onClick={handleGithubRedirect}>Projects</button>
         </div>
     )
 }
